438e2e2cf4c37b939ea2a7da4953a5b3261c49f5
Jess Pesale <jess_pesale@aol.com>
2021-06-30 16:16:47 -0400

    commit tree 81d16af98dc5af3a32226521fda0ea938f278c66

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+// const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, event) {
+    let thankYou = [ ]
+    for (let i=0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYou;
+}
+
+
+function countDown(integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer--
+    }
+console.log(integer);
+}
+
